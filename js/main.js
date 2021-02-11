92a40e7884fc3cf17bdd0c69006ba5a56170ea6d
Rackarn <22744125+MichaelEngdahl@users.noreply.github.com>
2021-02-11 12:48:40 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+import { setSearchFocus, showClearTextButton, clearSearchText, clearPushListener } from "./searchBar.js"
+import { getSearchTerm, retrieveSearchResults } from "./dataFunctions.js"
+import { deleteSearchResults, buildSearchResults, clearStatsLine, setStatsLine} from "./searchResults.js"
+
+document.addEventListener("readystatechange", (event) => {
+  if(event.target.readyState === "complete") {
+    initApp()
+  }
+})
+
+const initApp = () => {
+  setSearchFocus()
+
+  const search = document.getElementById("search")
+  search.addEventListener("input", showClearTextButton)
+  
+  const clear = document.getElementById("clear")
+  clear.addEventListener("click", clearSearchText)
+  clear.addEventListener("keydown", clearPushListener)
+
+  const form = document.getElementById("searchBar")
+  form.addEventListener("submit", submitTheSearch)
+
+  //const 
+}
+
+// Procedural "workflow" function (will call each func in order u state)
+const submitTheSearch = (event) => {
+  event.preventDefault()
+  deleteSearchResults()
+  // process the search
+  processTheSearch()
+
+  // set the focus
+  setSearchFocus()
+}
+
+// Procedural
+const processTheSearch = async () => {
+  clearStatsLine()
+  const searchTerm = getSearchTerm()
+  if(searchTerm === "") return
+  const resultArray = await retrieveSearchResults(searchTerm)
+  if(resultArray.length) buildSearchResults(resultArray)
+  setStatsLine(resultArray.length)
+}
+ 
